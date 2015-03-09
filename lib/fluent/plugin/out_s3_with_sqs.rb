951dc8dd283c5b4a68c5a0a5070ca60f4baa30bd
Scott Lau <scott@change.org>
2015-03-09 11:13:34 -0700

    Remove the word fail when the after-write hook runs.

--- lib/fluent/plugin/out_s3_with_sqs.rb
+++ lib/fluent/plugin/out_s3_with_sqs.rb
@@ -191,7 +191,7 @@ class S3WithSqsOutput < Fluent::TimeSlicedOutput
       @after_flush.each_with_index do |after_flush_cmd, i|
         config_file = @after_flush_config[i]
         after_hook_succeeded = system(after_flush_cmd, config_file, @s3_bucket, s3path, sqs_prefix)
-        log.info "After flush command \"#{after_flush_cmd} #{config_file} #{@s3_bucket} #{s3path} #{sqs_prefix}\" #{@tag} failed!" unless after_hook_succeeded
+        log.info "After flush command \"#{after_flush_cmd} #{config_file} #{@s3_bucket} #{s3path} #{sqs_prefix}\" #{@tag} exited with non-zero status, ignoring." unless after_hook_succeeded
       end
     ensure
       tmp.close(true) rescue nil
